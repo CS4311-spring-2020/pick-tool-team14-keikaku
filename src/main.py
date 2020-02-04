ad1d7454b37faf8ec50c6bc12ed7ef98cf401c95
Anthony DesArmier <ardesarmier@miners.utep.edu>
2020-02-04 16:48:50 -0700

    Corrected variable names, added lead status memory and vector sync context.
    Signed-off-by: Anthony DesArmier <ardesarmier@miners.utep.edu>

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QCheckBox
 from PyQt5.uic import loadUi
+from src import team_config
 from src.change_config import UiChangeConfig
 from src.directory_config import UiDirectoryConfig
 from src.event_config import UiEventConfig
@@ -20,73 +21,77 @@ class Ui(QMainWindow):
 
         self.teamButton = self.findChild(QPushButton, 'teamButton')
         self.teamButton.clicked.connect(self.execute_team_config)
-        self.teamButton = self.findChild(QPushButton, 'eventButton')
-        self.teamButton.clicked.connect(self.execute_event_config)
-        self.teamButton = self.findChild(QPushButton, 'vectorButton')
-        self.teamButton.clicked.connect(self.execute_vector_config)
-        self.teamButton = self.findChild(QPushButton, 'filterButton')
-        self.teamButton.clicked.connect(self.execute_filter_config)
-        self.teamButton = self.findChild(QPushButton, 'commitButton')
-        self.teamButton.clicked.connect(self.execute_change_config)
-        self.teamButton = self.findChild(QPushButton, 'syncButton')
-        if True:  # TODO: Find lead checkbox state
-            self.teamButton.clicked.connect(self.execute_vector_analyst)
-        else:
-            self.teamButton.clicked.connect(self.execute_vector_analyst)
-        self.teamButton = self.findChild(QPushButton, 'exportButton')
-        self.teamButton.clicked.connect(self.execute_export_config)
-        self.teamButton = self.findChild(QPushButton, 'directoryButton')
-        self.teamButton.clicked.connect(self.execute_directory_config)
-        self.teamButton = self.findChild(QPushButton, 'iconButton')
-        self.teamButton.clicked.connect(self.execute_icon_config)
-        self.teamButton = self.findChild(QPushButton, 'relationshipsButton')
-        self.teamButton.clicked.connect(self.execute_relationship_config)
+        self.eventButton = self.findChild(QPushButton, 'eventButton')
+        self.eventButton.clicked.connect(self.execute_event_config)
+        self.vectorButton = self.findChild(QPushButton, 'vectorButton')
+        self.vectorButton.clicked.connect(self.execute_vector_config)
+        self.filterButton = self.findChild(QPushButton, 'filterButton')
+        self.filterButton.clicked.connect(self.execute_filter_config)
+        self.commitButton = self.findChild(QPushButton, 'commitButton')
+        self.commitButton.clicked.connect(self.execute_change_config)
+        self.syncButton = self.findChild(QPushButton, 'syncButton')
+        self.syncButton.clicked.connect(self.execute_vector_db)
+        self.exportButton = self.findChild(QPushButton, 'exportButton')
+        self.exportButton.clicked.connect(self.execute_export_config)
+        self.directoryButton = self.findChild(QPushButton, 'directoryButton')
+        self.directoryButton.clicked.connect(self.execute_directory_config)
+        self.iconButton = self.findChild(QPushButton, 'iconButton')
+        self.iconButton.clicked.connect(self.execute_icon_config)
+        self.relationshipButton = self.findChild(QPushButton, 'relationshipsButton')
+        self.relationshipButton.clicked.connect(self.execute_relationship_config)
 
         self.show()
 
     def execute_change_config(self):
-        self.window = UiChangeConfig()
-        self.window.show()
+        self.change_window = UiChangeConfig()
+        self.change_window.show()
 
     def execute_directory_config(self):
-        self.window = UiDirectoryConfig()
-        self.window.show()
+        self.directory_window = UiDirectoryConfig()
+        self.directory_window.show()
 
     def execute_event_config(self):
-        self.window = UiEventConfig()
-        self.window.show()
+        self.event_window = UiEventConfig()
+        self.event_window.show()
 
     def execute_export_config(self):
-        self.window = UiExportConfig()
-        self.window.show()
+        self.export_window = UiExportConfig()
+        self.export_window.show()
 
     def execute_filter_config(self):
-        self.window = UiFilterConfig()
-        self.window.show()
+        self.filter_window = UiFilterConfig()
+        self.filter_window.show()
 
     def execute_icon_config(self):
-        self.window = UiIconConfig()
-        self.window.show()
+        self.icon_window = UiIconConfig()
+        self.icon_window.show()
 
     def execute_relationship_config(self):
-        self.window = UiRelationshipConfig()
-        self.window.show()
+        self.relationship_window = UiRelationshipConfig()
+        self.relationship_window.show()
 
     def execute_team_config(self):
-        self.window = UiTeamConfig()
-        self.window.show()
+        self.team_window = UiTeamConfig()
+        self.team_window.show()
 
     def execute_vector_config(self):
-        self.window = UiVectorConfig()
-        self.window.show()
+        self.vector_window = UiVectorConfig()
+        self.vector_window.show()
+
+    def execute_vector_db(self):
+        if team_config.lead_status:
+            self.vector_db_window = UiVectorDBLead()
+        else:
+            self.vector_db_window = UiVectorDBAnalyst()
+        self.vector_db_window.show()
 
     def execute_vector_analyst(self):
-        self.window = UiVectorDBAnalyst()
-        self.window.show()
+        self.vector_analyst_window = UiVectorDBAnalyst()
+        self.vector_analyst_window.show()
 
     def execute_vector_lead(self):
-        self.window = UiVectorDBLead()
-        self.window.show()
+        self.vector_lead_window = UiVectorDBLead()
+        self.vector_lead_window.show()
 
 
 if __name__ == "__main__":
--- src/team_config.py
+++ src/team_config.py
@@ -1,11 +1,22 @@
-from PyQt5.QtWidgets import QApplication, QDialog
+from PyQt5.QtWidgets import QApplication, QDialog, QCheckBox
 from PyQt5.uic import loadUi
+
+lead_status = 0
+
+
+def toggle_lead():
+    global lead_status
+    lead_status = lead_status ^ 2
 
 
 class UiTeamConfig(QDialog):
     def __init__(self):
         super(UiTeamConfig, self).__init__()
         loadUi('../ui/team_config.ui', self)
+
+        self.leadCheckBox.setCheckState(lead_status)
+        self.leadCheckBox = self.findChild(QCheckBox, 'leadCheckBox')
+        self.leadCheckBox.stateChanged.connect(toggle_lead)
 
         self.show()
 
