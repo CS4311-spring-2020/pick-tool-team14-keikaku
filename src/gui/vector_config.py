493184647b14ce065a4b3c75ce469bd1a8939c8b
David Rayner <darayner@miners.utep.edu>
2020-02-15 21:55:18 -0700

    vector configuration table add and remove entry functionality
    Signed-off-by: David Rayner <darayner@miners.utep.edu>

--- src/gui/vector_config.py
+++ src/gui/vector_config.py
@@ -12,7 +12,7 @@ __author__ = "Team Keikaku"
 __version__ = "0.1"
 
 import os
-from PyQt5.QtWidgets import QApplication, QFrame, QTableWidget
+from PyQt5.QtWidgets import QApplication, QFrame, QTableWidget, QPushButton
 from PyQt5.uic import loadUi
 from definitions import UI_PATH
 
@@ -30,11 +30,34 @@ class UiVectorConfig(QFrame):
         super(UiVectorConfig, self).__init__()
         loadUi(os.path.join(UI_PATH, 'vector_config.ui'), self)
 
-        self.iconTable = self.findChild(QTableWidget, 'vectorTable')
-        self.iconTable.setColumnWidth(0, 120)
-        self.iconTable.setColumnWidth(1, 120)
+        self.vectorTable = self.findChild(QTableWidget, 'vectorTable')
+        self.vectorTable.setColumnWidth(0, 120)
+        self.vectorTable.setColumnWidth(1, 120)
 
         self.show()
+
+        self.addVectorButton = self.findChild(QPushButton, 'addVectorButton')
+        self.addVectorButton.setShortcut("Ctrl+Return")
+        self.addVectorButton.clicked.connect(self.__add_row)
+        self.deleteVectorButton = self.findChild(QPushButton, 'deleteVectorButton')
+        self.deleteVectorButton.setShortcut("Ctrl+Backspace")
+        self.deleteVectorButton.clicked.connect(self.__delete_row)
+
+    def __add_row(self):
+        """Insert new vector entry on vector table"""
+
+        row_position = self.vectorTable.rowCount()
+        self.vectorTable.insertRow(row_position)
+
+
+    def __delete_row(self):
+        """Remove selected table entry from vector table"""
+
+        indexes = self.vectorTable.selectionModel().selectedRows()
+        if indexes is not None:
+            for index in sorted(indexes):
+                self.vectorTable.removeRow(index.row())
+
 
 
 if __name__ == "__main__":
