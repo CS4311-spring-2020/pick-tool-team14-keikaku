00898af5fd87399dedb461d3e56dd4142b56f44a
CommentSense <valentin.oneiro@gmail.com>
2020-02-27 19:31:19 -0700

    initial push for graph editor

--- src/gui/graph/graph_editor_window.py
+++ src/gui/graph/graph_editor_window.py
@@ -1,8 +1,10 @@
+import sys
 from PyQt5.QtWidgets import QWidget
 from PyQt5.QtWidgets import QVBoxLayout
 from PyQt5.QtWidgets import QGraphicsView
 from PyQt5.QtWidgets import QGraphicsScene
-
+from PyQt5.QtWidgets import QApplication
+from src.gui.graph.node_graphics_scene import QNodeGraphicsScence
 
 class GraphEditorWindow(QWidget):
     def __init__(self, parent=None):
@@ -10,16 +12,16 @@ class GraphEditorWindow(QWidget):
         self.init_ui()
 
     def init_ui(self):
-        self.setGeometry(200, 200, 200)
+        self.setGeometry(200, 200, 200, 600)
 
         # Stretches graphics view
         self.layout = QVBoxLayout()
         # Removes content margins
-        self.layout.setContentsMargins(0, 0, 0)
+        self.layout.setContentsMargins(0, 0, 0, 0)
         self.setLayout(self.layout)
 
         # Create graphics scene
-        self.grScence = QGraphicsScene()
+        self.grScence = QNodeGraphicsScence()
 
         # Create graphics view
         self.view = QGraphicsView(self)
@@ -28,3 +30,9 @@ class GraphEditorWindow(QWidget):
 
         self.setWindowTitle("Graph Editor")
         self.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    wnd = GraphEditorWindow()
+
+    sys.exit(app.exec_())
